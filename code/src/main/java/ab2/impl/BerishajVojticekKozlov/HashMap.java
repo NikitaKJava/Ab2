776acd18712e5e5b43ec2512f92ccfab83b45345
NiKo <80517411+NikitaKJava@users.noreply.github.com>
2023-05-25 20:15:41 +0200

    update

--- code/src/main/java/ab2/impl/BerishajVojticekKozlov/HashMap.java
+++ code/src/main/java/ab2/impl/BerishajVojticekKozlov/HashMap.java
@@ -2,6 +2,9 @@ package ab2.impl.BerishajVojticekKozlov;
 
 import ab2.AlgoDatHashMap;
 
+import java.util.Arrays;
+import java.util.Random;
+
 class Entry<K, V> {
     private final K key;
     private V value;
@@ -12,6 +15,12 @@ class Entry<K, V> {
         this.value = value;
         this.next = null;
     }
+
+//    Entry(K key, V value, Entry<K, V> next) {
+//        this.key = key;
+//        this.value = value;
+//        this.next = next;
+//    }
 
     public K getKey() {
         return key;
@@ -33,18 +42,30 @@ class Entry<K, V> {
         this.next = next;
     }
 }
-public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
-    private static int SIZE;
-    private static final int CAPACITY = 16;
-    private  Entry<K,V>[] entries;
 
-    public HashMap() {
-        entries = new Entry[CAPACITY];
+/**
+ * Creates new array
+ * @param <K>
+ * @param <V>
+ */
+public class HashMap<K,V> implements AlgoDatHashMap<K,V> {
+    private static int SIZE;
+    private static int CAPACITY;
+    private Entry<K, V>[] entries;
+
+    /**
+     * Constructor for HashMap with entries array
+     *
+     * @param capacity size of an array
+     */
+    public HashMap(int capacity) {
+        CAPACITY = capacity;
+        this.entries = new Entry[CAPACITY];
         SIZE = 0;
     }
 
     private int hash(K key) {
-        return key.hashCode() % entries.length;
+        return Math.abs(key.hashCode() % entries.length);
     }
 
     private void resize() {
@@ -66,13 +87,13 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
     public void clear() {
 
     }
+
     /**
      * Liefert true, wenn der Schlüssel key in der Hashtabelle vorhanden ist.
      *
-     * @param key
-     *            der zu suchende Schlüssel
+     * @param key der zu suchende Schlüssel
      * @return true, wenn der Schlüssel in der Hashtabelle vorhanden ist. Sonst
-     *         false.
+     * false.
      */
     @Override
     public boolean containsKey(K key) {
@@ -87,14 +108,14 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
         }
         return false;
     }
+
     /**
      * Liefert den Wert zu dem angegebenen Schlüssel. Ist der Schlüssel nicht
      * vorhanden, ist null zurück zu geben.6
      *
-     * @param key
-     *            der zu suchende Schlüssel
+     * @param key der zu suchende Schlüssel
      * @return Wert, der mit dem Schlüssel assoziiert ist. Ist der Schlüssel
-     *         nicht vorhanden, wird null zurück gegeben.
+     * nicht vorhanden, wird null zurück gegeben.
      */
     @Override
     public V get(K key) {
@@ -109,23 +130,24 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
         }
         return null;
     }
+
     /**
-     *
      * @return true, wenn die Hashtabelle leer ist (d.h. keine Elemente enthält)
      */
     @Override
     public boolean isEmpty() {
         return SIZE == 0;
     }
+
     /**
-     *
      * @return true, wenn die Hashtabelle voll ist (d.h. es können keine
-     *         weiteren Elemente mehr einfügt werden)
+     * weiteren Elemente mehr einfügt werden)
      */
     @Override
     public boolean isFull() {
         return CAPACITY == SIZE;
     }
+
     /**
      * Liefert ein Set, welches alle Schlüssel enthält, die in der Hashtabelle
      * gespeichert sind.
@@ -147,41 +169,40 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
         }
         return keysArray;
     }
+
     /**
      * Fügt einen Wert mit dem angegebenen Schlüssel in die Hashtabelle ein. Ein
      * bereits bestehendes Element mit dem selben Schlüssel (equals-Methode!) wird
      * dabei überschrieben und zurückgegeben.
      *
-     * @param key
-     *            der einzufügende Schlüssel
-     * @param value
-     *            der einzufülgende Wert
+     * @param key   der einzufügende Schlüssel
+     * @param value der einzufülgende Wert
      * @return null, wenn unter dem Schlüssel noch kein Wert eingefügt war.
-     *         Liefert das bisher gespeicherte Element, wenn mit dem Key schon
-     *         ein Wert vorhanden war.
-     * @throws IllegalStateException
-     *             Wenn kein freier Platz mehr in der Hashtabelle vorhanden ist
+     * Liefert das bisher gespeicherte Element, wenn mit dem Key schon
+     * ein Wert vorhanden war.
+     * @throws IllegalStateException Wenn kein freier Platz mehr in der Hashtabelle vorhanden ist
      */
     @Override
     public V put(K key, V value) throws IllegalStateException {
-        if(isFull()) {
+        if (isFull()) {
             throw new IllegalStateException("Kein freier Platz mehr in der Hashtabelle vorhanden");
         }
         int index = hash(key);
 
-        if (entries[index] == null) {
+        if (entries[index] == null) { // if Node null (empty), insert value
             entries[index] = new Entry<>(key, value);
             SIZE++;
             return null;
-        } else {
+        } else { // if not, create new entry
             Entry<K, V> current = entries[index];
-            while (current != null) {
+            while (current != null) { // if key not null, check for right index
                 if (current.getKey().equals(key)) {
                     current.setValue(value);
                     return null;
                 }
-                current = current.getNext();
+                current = current.getNext(); // if not the right index, create next Node
             }
+            // if next Node is null, create and put new key and value
             Entry<K, V> newEntry = new Entry<>(key, value);
             newEntry.setNext(entries[index]);
             entries[index] = newEntry;
@@ -193,6 +214,7 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
         }
         return null;
     }
+
     /**
      * Liefert die Anzahl an Werten, die in der Hashtabelle gespeichert sind.
      *
@@ -202,6 +224,7 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
     public int size() {
         return SIZE;
     }
+
     /**
      * Liefert die Größe der Hashtabelle zurück
      *
@@ -211,6 +234,7 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
     public int capacity() {
         return CAPACITY;
     }
+
     /**
      * Liefert eine Sammlung der in der Hashtabelle enthaltenen Werte.
      *
@@ -218,7 +242,7 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
      */
     @Override
     public V[] values() {
-        V[] valuesArray = (V[]) new Object[CAPACITY];
+        V[] valuesArray = (V[]) new Object[SIZE];
         int index = 0;
 
         for (Entry<K, V> entry : entries) {
@@ -229,23 +253,51 @@ public class HashMap<K,V> implements AlgoDatHashMap<K,V>{
                 index++;
             }
         }
-
         return valuesArray;
     }
 
-    public static <V, K> HashMap<K, V> linear() {
-        HashMap<K,V>[] hashMap = new HashMap<K, V>[CAPACITY];
-        int index = 0;
+//    public static <V, K> HashMap<K, V> linear(int minSize) {
+//        HashMap<K, V>[] hashMap = new HashMap[minSize];
+//        int index = 0;
+//
+//        for (HashMap<K, V> entry : hashMap) {
+//
+//            index++;
+//
+//        }
+//
+////        return hashMap;
+//        return null;
+//    }
 
-        for (HashMap<K, V> entry : hashMap) {
-            HashMap<K, V> current = entry;
-            while (current == null) {
-                hashMap[index] = current.getValue();
-                current = current.getNext();
-                index++;
-            }
+
+
+    public static void main(String[] args) {
+        AlgoDatHashMap<Integer, String> hm = new HashMap<>(12);
+//        checkFullMap(hm);
+//        printing(hm);
+//        System.out.println("It's full?: " + hm.isFull());
+
+        Random rand = new Random(System.currentTimeMillis());
+
+//        hm.put(0, "1");
+        int keyk = rand.nextInt(hm.capacity());
+
+//        System.out.println(Arrays.toString(hm.values()));
+        System.out.println(keyk);
+    }
+
+    public static void checkFullMap(AlgoDatHashMap<Integer, String> hashMap) {
+        char c = 'A';
+        for(int i = 0; i < hashMap.capacity(); i++) {
+            hashMap.put(i, i + String.valueOf(c));
+            c++;
         }
+    }
 
-        return hashMap;
+    public static void printing(AlgoDatHashMap<Integer, String> hashMap) {
+        for (int i = 0; i < hashMap.capacity(); i++) {
+            System.out.print("KEY: " + i + ", VALUE: " + hashMap.get(i) + ", ");
+        }
     }
 }
