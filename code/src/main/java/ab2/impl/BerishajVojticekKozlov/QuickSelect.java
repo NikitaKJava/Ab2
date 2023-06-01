c395eae665e53d087cc30ec3c67c1cb6ab2bdc2a
florianvo6 <florian.vojticek@gmx.at>
2023-06-01 19:57:52 +0200

    Update

--- code/src/main/java/ab2/impl/BerishajVojticekKozlov/QuickSelect.java
+++ code/src/main/java/ab2/impl/BerishajVojticekKozlov/QuickSelect.java
@@ -8,11 +8,70 @@ public class QuickSelect {
             throw new IllegalArgumentException("Array cannot be empty or null.");
         }
 
-        // Aufteilung von a in 5-er Gruppen
-        int[] subarray = new int[data.length/5];
-        int[] b = new int[5];
-
-
-        return 0;
+        if (i < 1 || i > data.length) {
+            throw new IllegalArgumentException("Der Idex liegt außerhalb des gültigen Bereichs");
+        }
+        return quickselectMax(data, 0, data.length - 1, i);
     }
-}
+
+        private int quickselectMax(int[] data, int low, int high, int i) {
+            if (low == high) {
+                return data[low];
+            }
+
+            int pivotIndex = getPivotIndex(data, low, high);
+            int rank = pivotIndex - low + 1;
+
+            if (i == rank) {
+                return data[pivotIndex];
+            } else if (i < rank) {
+                return quickselectMax(data, low, pivotIndex - 1, i);
+            } else {
+                return quickselectMax(data, pivotIndex + 1, high, i - rank);
+            }
+        }
+
+        private int getPivotIndex(int[] data, int low, int high){
+                if (high - low < 5) {
+                    return partition(data, low, high);
+                }
+
+                int numMedians = (high - low + 1) / 5;
+                int[] medians = new int[numMedians];
+
+                for (int i = 0; i < numMedians; i++) {
+                    int medianStart = low + i * 5;
+                    int medianEnd = medianStart + 4;
+                    medians[i] = partition(data, medianStart, medianEnd);
+                }
+
+                return quickselectMax(medians, 0, numMedians - 1, numMedians / 2);
+            }
+
+            private int partition(int[] data, int low, int high){
+                int pivot = data[high];
+                int i = low -1;
+
+                for(int j= low; j<high; j++){
+                    if(data[j]>= pivot){
+                        i++;
+                        swap(data, i, j);
+                    }
+                }
+
+                swap(data, i+1, high);
+                return i+1;
+            }
+
+    private void swap(int[] data,int i, int j) {
+                int temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+
+
+
+
+
+
+    }
